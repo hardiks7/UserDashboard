d3e7ebf3ad1212595c2cbc7540214680347761c2
hardy <hardipsinhp780@gmail.com>
2023-11-30 17:31:00 +0530

    Update User.jsx

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -109,29 +109,39 @@ const User = () => {
   };
 
 
-
-  const [userdata, setUserdata] = useState([])
-  // const [dialog, setDialog] = useState(false)
-  // const [dialogtitle, setDialogtitle] = useState('Add City')
-  // const [viewDialog, setViewDialog] = useState(false)
-  // const [deleteDialog, setDeleteDialog] = useState(false)
-  const [countryname, setCountryName] = useState("")
-  const [statename, setStateName] = useState("")
-  const [cityname, setCityName] = useState("")
-  // const [isdisabled, setIsDisabled] = useState(true)
+  
+  const [dialog, setDialog] = useState(false)
+  const [dialogtitle, setDialogtitle] = useState('Add City')
+  const [viewDialog, setViewDialog] = useState(false)
+  const [isdisabled, setIsDisabled] = useState(true)
   const [cityid, setCityId] = useState("")
   const [state, setState] = useState([])
   const [country, setCountry] = useState([])
-  // const [selectedCountryId, setselectedCountryId] = useState("")
-  // const [selectedStateId, setselectedStateId] = useState("")
+  const [selectedCountryId, setselectedCountryId] = useState("")
+  const [selectedStateId, setselectedStateId] = useState("")
+
+  const [userdata, setUserdata] = useState([])
   const [page, setPage] = useState(0)
-  const [rowsPerPage, setRowsPerPage] = useState(3)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
   const [totalrecord, setTotalRecord] = useState('')
   const [search, setSearch] = useState("")
   const [sortOrder, setSortOrder] = useState("asc")
+  const [deleteDialog, setDeleteDialog] = useState(false)
   const [userId, setUserID] = useState("")
-
+  const [firstname, setFirstName] = useState("")
+  const [LastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phonenumber, setPhoneNumber] = useState("")
+  const [profilepicture, setProfilePicture] = useState("")
+  const [resume, setResume] = useState("")
+  const [address, setAddress] = useState("")
+  const [countryname, setCountryName] = useState("")
+  const [statename, setStateName] = useState("")
+  const [cityname, setCityName] = useState("")
+  const [imageDiloge, setImageDialog] = useState(false)
+  
   const count = parseInt(totalrecord);
+
 
   useEffect(() => {
     if (selectedCountryId === '' || selectedStateId === '' || cityname === '') {
@@ -333,18 +343,15 @@ const User = () => {
       })
   }
 
-  // pagination
+  // Pagination
 
-  const getUser = useCallback(async (pages, rowsPerPage) => {
+  const getUserData = useCallback(async (pages, rowsPerPage) => {
     const response = await fetch(`http://localhost:4500/UserPagination?pageNumber=${pages + 1}&pageSize=${rowsPerPage}&filter=${search}&sortOrder=${sortOrder}`)
     const data = await response.json();
     setUserdata(data.data)
     setTotalRecord(data.total)
   }, [search, sortOrder])
 
-  // useEffect(() => {
-  // getCity(0,rowsPerPage);
-  // }, [getCity])
 
   useEffect(() => {
     if (!sessionStorage.getItem("token")) {
@@ -415,7 +422,7 @@ const User = () => {
                 noWrap
                 sx={{ flexGrow: 1 }}
               >
-                City Page
+                User Page
               </Typography>
 
               <Button variant="contained"
@@ -423,7 +430,7 @@ const User = () => {
                 onClick={handleBack}>
                 Go To Dashboard
               </Button>
-
+              
             </Toolbar>
           </AppBar>
           <Drawer variant="permanent" open={open}>
@@ -493,7 +500,7 @@ const User = () => {
               fontSize:'20px',
               borderRadius:'5px'
             }}
-                onClick={() => handleAdd('add')}> Add <AddIcon sx={{
+                onClick={() => handleAdd('add')}> Add USer <AddIcon sx={{
                   paddingTop: '5px',
                   fontSize: '20px'
                 }} />
@@ -501,10 +508,9 @@ const User = () => {
             </Box>
 
             <Dialog
-              open={dialog}
+              open={viewDialog}
               TransitionComponent={Transition}
               keepMounted
-              // onClose={handleClose}
               aria-describedby="alert-dialog-slide-description"
             >
               <DialogTitle
@@ -513,54 +519,66 @@ const User = () => {
                   onClick={handleClose}> X </Button>
               </DialogTitle>
               <DialogContent>
-                
-                  <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', width: '30rem' }}>
-                    <div>
-                      <InputLabel id="demo-simple-select-standard-label">Select Country</InputLabel>
-                      <Select
-                        labelId="demo-simple-select-standard-label"
-                        id="demo-simple-select-standard"
-                        label="Select Country"
-                        variant='standard'
-                        sx={{ width: "30rem" }}
-                        value={selectedCountryId}
-                        onChange={(e) => setselectedCountryId(e.target.value)}
-                      >
-                        {country.map((countries, index) => (
-                          <MenuItem key={index} value={countries.countryid}>{countries.countryname}</MenuItem>
-                        ))}
-                      </Select>
-                    </div>
-                    <div>
-                      <InputLabel id="demo-simple-select-standard-label">Select State</InputLabel>
-                      <Select
-                        labelId="demo-simple-select-standard-label"
-                        id="demo-simple-select-standard"
-                        label="Select State"
-                        variant='standard'
-                        sx={{ width: "30rem" }}
-                        value={selectedStateId}
-                        onChange={(e) => setselectedStateId(e.target.value)}
-                      >
-                        {state.map((state, index) => (
-                          <MenuItem key={index} value={state.stateid}>{state.statename}</MenuItem>
-                        ))}
-                      </Select>
-                    </div>
-                    <div>
-                      <TextField label="CityName" variant='standard'
-                        value={cityname} onChange={(e) => setCityName(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))} fullWidth />
-                    </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "1px",
+                    width: "30rem",
+                    fontFamily: "cursive",
+                    fontSize: '20px'
+                  }}
+                >
+                  <div style={{ paddingTop: "5px" }}>
+                    <p>FirstName: <span>{firstname}</span></p>
                   </div>
+                  <div>
+                    <p>LastName: <span>{lastname}</span></p>
+                  </div>
+                  <div>
+                    <p>E-Mail: <span>{email}</span></p>
+                  </div>
+                  <div>
+                    <p>PhoneNumber: <span>{phonenumber}</span></p>
+                  </div>
+                  <div style={{marginTop: '1rem'}}>
+                    <span>ProfilePicture: </span> <br /><img src={profilepicture}  width='70px'/>
+                  </div>
+                  <div>
+                    <p>Resume: <span>{resume}</span></p>
+                  </div>
+                  <div>
+                    <p>Address: <span>{address}</span></p>
+                  </div>
+                  <div>
+                    <p>CountryName: <span>{countryname}</span></p>
+                  </div>
+                  <div>
+                    <p>StateName: <span>{statename}</span></p>
+                  </div>
+                  <div>
+                    <p>CityName: <span>{cityname}</span></p>
+                  </div>
+                </div>
               </DialogContent>
-              <DialogActions>
+              </Dialog>
+            <Dialog
+              open={imageDialog}
+              TransitionComponent={Transition}
+              keepMounted
+              aria-describedby="alert-dialog-slide-description"
+              onClose={() => setImageDialog(false)}
+            >
+                <img src={profilepicture}  width='100%'/>
+            </Dialog>
+              {/* <DialogActions>
                 {dialogtitle === 'Add City' ? (<Button disabled={isdisabled} onClick={(e) => handelsubmit(e)}>SUBMIT</Button>)
                   : (<Button disabled={isdisabled} onClick={(e) => handleupdate(e, cityid)}>Update</Button>)}
                 <Button onClick={handleClear}>CANCEL</Button>
-              </DialogActions>
-            </Dialog>
+              </DialogActions> */}
+            
 
-            <Dialog
+            {/* <Dialog
               open={viewDialog}
               TransitionComponent={Transition}
               keepMounted
@@ -581,7 +599,7 @@ const User = () => {
                   <p>{cityname}</p>
                 </div>
               </DialogContent>
-            </Dialog>
+            </Dialog> */}
 
             <Dialog
               open={deleteDialog}
@@ -595,8 +613,8 @@ const User = () => {
                 <h4> Are You Sure You Want To Delete This City</h4>
               </DialogContent>
               <DialogActions>
-                <Button onClick={() => handleDeleteConfirm(cityid)}>DELETE</Button>
-                <Button onClick={handleClose}>CANCEL</Button>
+                <Button onClick={() => handleDeleteConfirm(cityid)}> DELETE </Button>
+                <Button onClick={handleClose}> CANCEL </Button>
               </DialogActions>
             </Dialog>
 
@@ -629,8 +647,8 @@ const User = () => {
                         <TableCell ><img src={data.profilepicture} width= '65px' height= '65px' /></TableCell>
                         <TableCell >{data.resume}</TableCell>
                         <TableCell><ModeEditOutlineIcon sx={{ color: '#3f533d' }} onClick={() => handleEdit('edit', city)} /></TableCell>
-                        <TableCell><DeleteIcon sx={{ color: '#724560' }} onClick={() => { handleDelete(city) }}></DeleteIcon></TableCell>
-                        <TableCell><RemoveRedEyeIcon sx={{ color: '#5c7262' }} onClick={() => { handleView(city) }} /></TableCell>
+                        <TableCell><DeleteIcon sx={{ color: '#724560' }} onClick={() => { handleDelete(data) }}></DeleteIcon></TableCell>
+                        <TableCell><RemoveRedEyeIcon sx={{ color: '#5c7262' }} onClick={() => { handleView(data.userid) }} /></TableCell>
                       </TableRow>
                     ))}
                   </TableBody>
